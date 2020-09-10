a7ec1f1e1cc0492a11a9246828f2941e672da4ba
PJ <pjjoh1@gmail.com>
2020-09-10 11:54:59 +1000

    pjbrach commit

---
+++ phython2.py
@@ -0,0 +1,31 @@
+
+import requests
+import json
+
+print("Hello World")
+# adding comment
+#a = input("what is the meaning of life? ")
+
+#print(a)
+
+l = ['a', 'b']
+
+#print(l[0])
+#print(l[1])
+
+l.append('c')
+
+#print(l)
+
+m =[]
+
+for num in range(1,12):
+	m.append(num)
+print(m)
+
+n=[]
+num =0
+while num<12:
+	num = num+1
+	n.append(num)
+# print(n)
